e8a7e487d6fdf6d5a4411100c4c3eb4e38ee764a
IsacJSantos <isac.santos@electricmonkeys.games>
2024-10-09 19:17:39 -0300

    Moving platform implemented

--- Assets/_Scripts/Platforms/MovingPlatform.cs
+++ Assets/_Scripts/Platforms/MovingPlatform.cs
@@ -1,12 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class MovingPlatform : Platform
 {
+    [SerializeField]
+    private Transform platform;
+    [SerializeField]
+    private Transform pointA;
+    [SerializeField]
+    private Transform pointB;
+
+    private Transform currentPoint;
+
+    [ContextMenu("Interact")]
     public override void Interact()
     {
-        
+        Move();
     }
+
+    private void Move()
+    {
+        platform.DOKill();
+        if (currentPoint == pointB)
+        {
+            currentPoint = pointA;
+            platform.DOMove(pointA.position, 0.8f).SetEase(Ease.InQuad).Play();
+        }
+        else
+        {
+            currentPoint = pointB;
+            platform.DOMove(pointB.position, 0.8f).SetEase(Ease.InQuad).Play();
+        }
+    }
+
 
 }
